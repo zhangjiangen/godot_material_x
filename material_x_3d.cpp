8bf46430bb11ed8ee31c15dbced6b067c116cd2e
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-03-10 02:40:30 -0800

    Format code.

--- material_x_3d.cpp
+++ material_x_3d.cpp
@@ -224,171 +224,138 @@ RES MTLXLoader::load(const String &p_path, const String &p_original_path, Error 
 	// Apply modifiers to the content document.
 	applyModifiers(doc, modifiers);
 
-    // Validate the document.
-    std::string message;
-    if (!doc->validate(&message))
-    {
-        std::cerr << "*** Validation warnings for "
-                  << materialFilename.getBaseName() << " ***" << std::endl;
-        std::cerr << message;
-        std::cout << message << std::endl;
-    }
+	// Validate the document.
+	std::string message;
+	if (!doc->validate(&message)) {
+		std::cerr << "*** Validation warnings for "
+				  << materialFilename.getBaseName() << " ***" << std::endl;
+		std::cerr << message;
+		std::cout << message << std::endl;
+	}
 
-    if (!doc)
-    {
-        return RES();
-    }
-    imageHandler->setSearchPath(searchPath);
+	if (!doc) {
+		return RES();
+	}
+	imageHandler->setSearchPath(searchPath);
 
-    // Compute baking resolution.
-    mx::ImageVec imageVec = imageHandler->getReferencedImages(doc);
-    auto maxImageSize = mx::getMaxDimensions(imageVec);
-    if (bakeWidth == -1)
-    {
-        bakeWidth = std::max(maxImageSize.first, (unsigned int) 4);
-    }
-    if (bakeHeight == -1)
-    {
-        bakeHeight = std::max(maxImageSize.second, (unsigned int) 4);
-    }
+	// Compute baking resolution.
+	mx::ImageVec imageVec = imageHandler->getReferencedImages(doc);
+	auto maxImageSize = mx::getMaxDimensions(imageVec);
+	if (bakeWidth == -1) {
+		bakeWidth = std::max(maxImageSize.first, (unsigned int)4);
+	}
+	if (bakeHeight == -1) {
+		bakeHeight = std::max(maxImageSize.second, (unsigned int)4);
+	}
 
-    // Construct a texture baker.
-    mx::Image::BaseType baseType =
-        bakeHdr ? mx::Image::BaseType::FLOAT : mx::Image::BaseType::UINT8;
-    mx::TextureBakerPtr baker =
-        mx::TextureBaker::create(bakeWidth, bakeHeight, baseType);
-    baker->setupUnitSystem(stdLib);
-    baker->setDistanceUnit(context.getOptions().targetDistanceUnit);
-    baker->setAverageImages(bakeAverage);
-    baker->setOptimizeConstants(bakeOptimize);
+	// Construct a texture baker.
+	mx::Image::BaseType baseType =
+			bakeHdr ? mx::Image::BaseType::FLOAT : mx::Image::BaseType::UINT8;
+	mx::TextureBakerPtr baker =
+			mx::TextureBaker::create(bakeWidth, bakeHeight, baseType);
+	baker->setupUnitSystem(stdLib);
+	baker->setDistanceUnit(context.getOptions().targetDistanceUnit);
+	baker->setAverageImages(bakeAverage);
+	baker->setOptimizeConstants(bakeOptimize);
 
-    // Assign our existing image handler, releasing any existing render
-    // resources for cached images.
-    imageHandler->releaseRenderResources();
-    baker->setImageHandler(imageHandler);
+	// Assign our existing image handler, releasing any existing render
+	// resources for cached images.
+	imageHandler->releaseRenderResources();
+	baker->setImageHandler(imageHandler);
 
-    // Find new renderable elements.
-    mx::StringVec renderablePaths;
-    std::vector<mx::TypedElementPtr> elems;
-    std::vector<mx::NodePtr> materialNodes;
-    mx::findRenderableElements(doc, elems);
-    if (elems.empty())
-    {
-        throw mx::Exception("No renderable elements found in " +
-                            materialFilename.getBaseName());
-    }
-    for (mx::TypedElementPtr elem : elems)
-    {
-        mx::TypedElementPtr renderableElem = elem;
-        mx::NodePtr node = elem->asA<mx::Node>();
-        if (node && node->getType() == mx::MATERIAL_TYPE_STRING)
-        {
-            std::vector<mx::NodePtr> shaderNodes = getShaderNodes(node);
-            if (!shaderNodes.empty())
-            {
-                renderableElem = shaderNodes[0];
-            }
-            materialNodes.push_back(node);
-        }
-        else
-        {
-            materialNodes.push_back(nullptr);
-        }
-        renderablePaths.push_back(renderableElem->getNamePath());
-    }
-    try
-    {
-        // Check for any udim set.
-        mx::ValuePtr udimSetValue = doc->getGeomPropValue("udimset");
+	// Find new renderable elements.
+	mx::StringVec renderablePaths;
+	std::vector<mx::TypedElementPtr> elems;
+	std::vector<mx::NodePtr> materialNodes;
+	mx::findRenderableElements(doc, elems);
+	if (elems.empty()) {
+		return RES();
+	}
+	for (mx::TypedElementPtr elem : elems) {
+		mx::TypedElementPtr renderableElem = elem;
+		mx::NodePtr node = elem->asA<mx::Node>();
+		if (node && node->getType() == mx::MATERIAL_TYPE_STRING) {
+			std::vector<mx::NodePtr> shaderNodes = getShaderNodes(node);
+			if (!shaderNodes.empty()) {
+				renderableElem = shaderNodes[0];
+			}
+			materialNodes.push_back(node);
+		} else {
+			materialNodes.push_back(nullptr);
+		}
+		renderablePaths.push_back(renderableElem->getNamePath());
+	}
+	try {
+		// Check for any udim set.
+		mx::ValuePtr udimSetValue = doc->getGeomPropValue("udimset");
 
-        // Create new materials.
-        mx::TypedElementPtr udimElement;
-        for (size_t i = 0; i < renderablePaths.size(); i++)
-        {
-            const auto& renderablePath = renderablePaths[i];
-            mx::ElementPtr elem = doc->getDescendant(renderablePath);
-            mx::TypedElementPtr typedElem =
-                elem ? elem->asA<mx::TypedElement>() : nullptr;
-            if (!typedElem)
-            {
-                continue;
-            }
-            if (udimSetValue && udimSetValue->isA<mx::StringVec>())
-            {
-                for (const std::string& udim : udimSetValue->asA<mx::StringVec>())
-                {
-                    MaterialPtr mat = MTLXMaterial::create();
-                    mat->setDocument(doc);
-                    mat->setElement(typedElem);
-                    mat->setMaterialNode(materialNodes[i]);
-                    mat->setUdim(udim);
-                    newMaterials.push_back(mat);
+		// Create new materials.
+		mx::TypedElementPtr udimElement;
+		for (size_t i = 0; i < renderablePaths.size(); i++) {
+			const auto &renderablePath = renderablePaths[i];
+			mx::ElementPtr elem = doc->getDescendant(renderablePath);
+			mx::TypedElementPtr typedElem =
+					elem ? elem->asA<mx::TypedElement>() : nullptr;
+			if (!typedElem) {
+				continue;
+			}
+			if (udimSetValue && udimSetValue->isA<mx::StringVec>()) {
+				for (const std::string &udim : udimSetValue->asA<mx::StringVec>()) {
+					MaterialPtr mat = MTLXMaterial::create();
+					mat->setDocument(doc);
+					mat->setElement(typedElem);
+					mat->setMaterialNode(materialNodes[i]);
+					mat->setUdim(udim);
+					newMaterials.push_back(mat);
 
-                    udimElement = typedElem;
-                }
-            }
-            else
-            {
-                MaterialPtr mat = MTLXMaterial::create();
-                mat->setDocument(doc);
-                mat->setElement(typedElem);
-                mat->setMaterialNode(materialNodes[i]);
-                newMaterials.push_back(mat);
-            }
-        }
+					udimElement = typedElem;
+				}
+			} else {
+				MaterialPtr mat = MTLXMaterial::create();
+				mat->setDocument(doc);
+				mat->setElement(typedElem);
+				mat->setMaterialNode(materialNodes[i]);
+				newMaterials.push_back(mat);
+			}
+		}
 
-        if (!newMaterials.empty())
-        {
+		if (!newMaterials.empty()) {
+			// Add new materials to the global vector.
+			materials.insert(materials.end(), newMaterials.begin(),
+					newMaterials.end());
 
-            // Add new materials to the global vector.
-            materials.insert(materials.end(), newMaterials.begin(),
-                             newMaterials.end());
+			MaterialPtr udimMaterial = nullptr;
+			for (MaterialPtr mat : newMaterials) {
+				mx::TypedElementPtr elem = mat->getElement();
 
-            MaterialPtr udimMaterial = nullptr;
-            for (MaterialPtr mat : newMaterials)
-            {
-                mx::TypedElementPtr elem = mat->getElement();
-
-                std::string udim = mat->getUdim();
-                if (!udim.empty())
-                {
-                    if ((udimElement == elem) && udimMaterial)
-                    {
-                        // Reuse existing material for all udims
-                        mat->copyShader(udimMaterial);
-                    }
-                    else
-                    {
-                        // Generate a shader for the new material.
-                        mat->generateShader(context);
-                        if (udimElement == elem)
-                        {
-                            udimMaterial = mat;
-                        }
-                    }
-                }
-                else
-                {
-                    // Generate a shader for the new material.
-                    mat->generateShader(context);
-                }
-            }
-        }
-    }
-    catch (mx::ExceptionRenderError& e)
-    {
-        for (const std::string& error : e.errorLog())
-        {
-            std::cerr << error << std::endl;
-        }
-        return RES();
-    }
-    catch (std::exception& e)
-    {
-        std::cout << "Failed to load material\n"
-                  << e.what() << std::endl;
-        return RES();
-    }	
+				std::string udim = mat->getUdim();
+				if (!udim.empty()) {
+					if ((udimElement == elem) && udimMaterial) {
+						// Reuse existing material for all udims
+						mat->copyShader(udimMaterial);
+					} else {
+						// Generate a shader for the new material.
+						mat->generateShader(context);
+						if (udimElement == elem) {
+							udimMaterial = mat;
+						}
+					}
+				} else {
+					// Generate a shader for the new material.
+					mat->generateShader(context);
+				}
+			}
+		}
+	} catch (mx::ExceptionRenderError &e) {
+		for (const std::string &error : e.errorLog()) {
+			std::cerr << error << std::endl;
+		}
+		return RES();
+	} catch (std::exception &e) {
+		std::cout << "Failed to load material\n"
+				  << e.what() << std::endl;
+		return RES();
+	}
 
 	// Bake all materials in the active document.
 	try {
