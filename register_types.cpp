280620d19a8293f2ec4bf600b3991f3a6784a254
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-03-11 19:28:32 -0800

    Use a static variable to hold materialx.

--- register_types.cpp
+++ register_types.cpp
@@ -2,12 +2,14 @@
 
 #include "material_x_3d.h"
 
+static Ref<MTLXLoader> resource_format_mtlx;
 
 void register_material_x_types() {
-	Ref<MTLXLoader> resource_format_mtlx;
 	resource_format_mtlx.instantiate();
 	ResourceLoader::add_resource_format_loader(resource_format_mtlx);
 }
 
 void unregister_material_x_types() {
+	ResourceLoader::remove_resource_format_loader(resource_format_mtlx);
+	resource_format_mtlx.unref();
 }
