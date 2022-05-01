e5257efc9377a47dc85df55bc8627303ef6ca52f
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-01 16:07:08 -0700

    Update MaterialX version.

--- source/MaterialXCore/Generated.h
+++ source/MaterialXCore/Generated.h
@@ -8,19 +8,19 @@
 
 #define MATERIALX_MAJOR_VERSION 1
 #define MATERIALX_MINOR_VERSION 38
-#define MATERIALX_BUILD_VERSION 4
+#define MATERIALX_BUILD_VERSION 5
 
 // Establish namespace:
-namespace MaterialX_v1_38_4
+namespace MaterialX_v1_38_5
 {
 }
 
 // Establish alias to allow downstream clients to still use the MaterialX namespace:
-namespace MaterialX = MaterialX_v1_38_4;
+namespace MaterialX = MaterialX_v1_38_5;
 
 // All code in this project must use these macros for opening and closing the
 // global MaterialX namespace:
-#define MATERIALX_NAMESPACE_BEGIN namespace MaterialX_v1_38_4 {
+#define MATERIALX_NAMESPACE_BEGIN namespace MaterialX_v1_38_5 {
 #define MATERIALX_NAMESPACE_END }
 
 #endif
