dbc4c70bfa5071e9b80e2290097ab7d4d6c4bc3d
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-05 20:41:04 -0700

    Update register module.

--- register_types.cpp
+++ register_types.cpp
@@ -4,13 +4,23 @@
 
 static Ref<MTLXLoader> resource_format_mtlx;
 
-void register_material_x_types() {
-	GDREGISTER_CLASS(MTLXLoader);
-	resource_format_mtlx.instantiate();
-	ResourceLoader::add_resource_format_loader(resource_format_mtlx);
+void initialize_material_x_module(ModuleInitializationLevel p_level)
+{
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+    {
+        return;
+    }
+    GDREGISTER_CLASS(MTLXLoader);
+    resource_format_mtlx.instantiate();
+    ResourceLoader::add_resource_format_loader(resource_format_mtlx);
 }
 
-void unregister_material_x_types() {
-	ResourceLoader::remove_resource_format_loader(resource_format_mtlx);
-	resource_format_mtlx.unref();
+void uninitialize_material_x_module(ModuleInitializationLevel p_level)
+{
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+    {
+        return;
+    }
+    ResourceLoader::remove_resource_format_loader(resource_format_mtlx);
+    resource_format_mtlx.unref();
 }
--- register_types.h
+++ register_types.h
@@ -1,2 +1,6 @@
-void register_material_x_types();
-void unregister_material_x_types();
+#pragma once
+
+#include "modules/register_module_types.h"
+
+void register_material_x_module(ModuleInitializationLevel p_level);
+void unregister_material_x_module(ModuleInitializationLevel p_level);
