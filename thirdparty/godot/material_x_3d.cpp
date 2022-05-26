ee61fc1545d4f385f1989cd11c4115c1a6b8b233
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-25 20:42:17 -0700

    Don't generate a new id.

--- thirdparty/godot/material_x_3d.cpp
+++ thirdparty/godot/material_x_3d.cpp
@@ -567,7 +567,7 @@ Ref<Resource> MTLXLoader::load(const String& p_path, const String& p_original_pa
     }
     config->set_value("remap", "importer", "material");
     config->set_value("remap", "type", "StandardMaterial3D");
-    config->set_value("remap", "uid", ResourceSaver::get_resource_id_for_path(p_original_path, true));
+    config->set_value("remap", "uid", ResourceSaver::get_resource_id_for_path(p_original_path, false));
     config->set_value("remap", "path", new_path);
     config->save(p_original_path.get_basename() + ".import");
     ResourceSaver::save(new_path, mat);
