433412533065827657830291da1b686e8b306908
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-25 20:32:46 -0700

    Order the config settings.

--- thirdparty/godot/material_x_3d.cpp
+++ thirdparty/godot/material_x_3d.cpp
@@ -565,10 +565,10 @@ Ref<Resource> MTLXLoader::load(const String& p_path, const String& p_original_pa
     {
         config->load(p_original_path.get_basename() + ".import");
     }
-    config->set_value("remap", "uid", ResourceSaver::get_resource_id_for_path(p_original_path, true));
-    config->set_value("remap", "path", new_path);
     config->set_value("remap", "importer", "material");
     config->set_value("remap", "type", "StandardMaterial3D");
+    config->set_value("remap", "uid", ResourceSaver::get_resource_id_for_path(p_original_path, true));
+    config->set_value("remap", "path", new_path);
     config->save(p_original_path.get_basename() + ".import");
     ResourceSaver::save(new_path, mat);
     return mat;
